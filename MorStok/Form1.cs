18a697b75f83bc3e49c72f276f23da67341e133a
Metincan Çakmak <rexlerpanz@gmail.com>
2024-10-17 22:36:01 +0300

    Proje dosyası ekle.

---
+++ MorStok/Form1.cs
@@ -0,0 +1,18 @@
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace MorStok
+{
+    public partial class MorStok : MaterialForm
+    {
+        public MorStok()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+    }
+}

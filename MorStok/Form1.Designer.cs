ff72fb71e7529f461a0a01e6ca25e7fd63a78053
Metincan Çakmak <rexlerpanz@gmail.com>
2024-10-17 23:49:18 +0300

    TabControl eklendi.

--- MorStok/Form1.Designer.cs
+++ MorStok/Form1.Designer.cs
@@ -28,18 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MorStok));
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            tabPage5 = new TabPage();
+            ımageList1 = new ImageList(components);
+            listView1 = new ListView();
+            tabControl1.SuspendLayout();
+            tabPage2.SuspendLayout();
             SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Controls.Add(tabPage5);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.ImageList = ımageList1;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1366, 768);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.ImageKey = "house-solid.png";
+            tabPage1.Location = new Point(4, 39);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1358, 725);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Anasayfa";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(listView1);
+            tabPage2.ImageKey = "list-solid.png";
+            tabPage2.Location = new Point(4, 39);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1358, 725);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Ürünler";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.ImageKey = "money-bill-solid.png";
+            tabPage3.Location = new Point(4, 39);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1358, 725);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Satış";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.ImageKey = "chart-simple-solid.png";
+            tabPage4.Location = new Point(4, 39);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(1358, 725);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Rapor";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            tabPage5.ImageKey = "gear-solid.png";
+            tabPage5.Location = new Point(4, 39);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Size = new Size(1358, 725);
+            tabPage5.TabIndex = 4;
+            tabPage5.Text = "Ayarlar";
+            tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // ımageList1
+            // 
+            ımageList1.ColorDepth = ColorDepth.Depth32Bit;
+            ımageList1.ImageStream = (ImageListStreamer)resources.GetObject("ımageList1.ImageStream");
+            ımageList1.TransparentColor = Color.Transparent;
+            ımageList1.Images.SetKeyName(0, "house-solid.png");
+            ımageList1.Images.SetKeyName(1, "diamond-solid.png");
+            ımageList1.Images.SetKeyName(2, "list-solid.png");
+            ımageList1.Images.SetKeyName(3, "plus-solid.png");
+            ımageList1.Images.SetKeyName(4, "chart-simple-solid.png");
+            ımageList1.Images.SetKeyName(5, "gear-solid.png");
+            ımageList1.Images.SetKeyName(6, "money-bill-solid.png");
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(8, 6);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(1344, 480);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
             // 
             // MorStok
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1366, 768);
+            Controls.Add(tabControl1);
+            DrawerShowIconsWhenHidden = true;
             Name = "MorStok";
+            Sizable = false;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "MorStok";
+            tabControl1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private ImageList ımageList1;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private ListView listView1;
     }
 }
